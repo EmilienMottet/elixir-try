1ce93c26e733717e227744282538acf029ecb015
Emilien Mottet <emilien.mottet@grenoble-inp.org>
2018-08-17 20:52:37 +0200

    add full doc

---
+++ math.ex
@@ -0,0 +1,14 @@
+defmodule Math do
+  def sum(a, b) do
+    do_sum(a, b)
+  end
+
+  defp do_sum(a, b) do
+    a + b
+  end
+
+  def zero?(0), do: true
+  def zero?(x) when is_integer(x), do: false
+end
+
+IO.puts(Math.sum(1, 2))
